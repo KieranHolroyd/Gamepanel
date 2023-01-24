01886807a632a96694797c08c1144bf4e78312c1
Uncle Scrooge <Ni1kko@outlook.com>
2023-01-24 19:46:03 +0000

    ❌Working but needs refactored ❌

--- db.php
+++ db.php
@@ -9,46 +9,41 @@ $dsn = 'mysql:host=' . $host . ';port=' . Config::$sql['port'] . ';dbname=' . $d
 $pdo = new PDO($dsn, $user, $password);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
-function game_pdo()
-{
-    $gamehost = Config::$gameSql['host'];
-    $gameuser = Config::$gameSql['user'];
-    $gamepassword = Config::$gameSql['pass'];
-    $gamedbname = Config::$gameSql['name'];
-    $gamedsn = 'mysql:host=' . $gamehost . ';port=' . Config::$gameSql['port'] . ';dbname=' . $gamedbname . ';charset=utf8';
-    static $_PDO = null;
-
-    if ($_PDO === null) {
-        $_PDO = new GAMEDB($gamedsn, $gameuser, $gamepassword);
-        $_PDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-    }
-
-    return $_PDO;
-}
 
 class GAMEDB extends PDO
 {
 
-    protected $_config = [];
+    protected $_config = [
+        'dsn' => "",
+        'user' => "",
+        'pass' => ""
+    ];
 
     protected $_connected = false;
 
-    public function __construct($dsn, $user = null, $pass = null, $options = null)
+    public function __construct($dsn = "", $user = "", $pass = "")
     {
-        parent::__construct($dsn, $user, $pass, $options);
-        //Save connection details for later
-        $this->_config = [
-            'dsn' => $dsn,
-            'user' => $user,
-            'pass' => $pass,
-            'options' => $options
-        ];
+        try {
+            //Save connection details for later
+        
+            $this->_config['dsn'] = 'mysql:host=' . Config::$gameSql['host'] . ';port=' . Config::$sql['port'] . ';dbname=' . Config::$gameSql['name'];
+            $this->_config['user'] = Config::$gameSql['user'];
+            $this->_config['pass'] = Config::$gameSql['pass'];
+        
+
+            parent::__construct($this->_config['dsn'], $this->_config['user'], $this->_config['pass']);
+            $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+           
+        }
+        catch(PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();;
+        }  
     }
 
     public function checkConnection()
     {
         if (!$this->_connected) {
-            parent::__construct($this->_config['dsn'], $this->_config['user'], $this->_config['pass'], $this->_config['options']);
+            parent::__construct($this->_config['dsn'], $this->_config['user'], $this->_config['pass']);
             $this->_connected = true;
         }
     }
@@ -72,4 +67,21 @@ class GAMEDB extends PDO
     }
 }
 
+function game_pdo()
+{
+    $gamehost = Config::$gameSql['host'];
+    $gameuser = Config::$gameSql['user'];
+    $gamepassword = Config::$gameSql['pass'];
+    $gamedbname = Config::$gameSql['name'];
+    $gamedsn = 'mysql:host=' . $gamehost . ';port=' . Config::$gameSql['port'] . ';dbname=' . $gamedbname . ";charset=utf8";
+    static $_PDO = null;
+
+    if ($_PDO === null) {
+        $_PDO = new GAMEDB($gamedsn, $gameuser, $gamepassword);
+    }
+
+    return $_PDO;
+}
+
+game_pdo();
 ?>
