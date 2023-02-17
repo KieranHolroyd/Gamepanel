540270738d119227fc29e701c2bf32fb6b14b51b
Kieran <kieran@nitrexdesign.co.uk>
2023-02-17 23:43:05 +0000

    update wrong error message on nostaff page

--- errors/nostaff.php
+++ errors/nostaff.php
@@ -5,6 +5,7 @@
         overflow: hidden;
         padding: 0 0 !important;
     }
+
     .navSlider {
         top: 0;
     }
@@ -13,8 +14,8 @@
     <h1 class="title">This is a little awkward...</h1>
     <p>You're not staff, So you can only access a few pages.</p>
     <button style="position: absolute; top: 0; right: 0;border-radius: .25em;" onclick="window.logout()">Logout</button>
-    <a href="https://arma-life.com">
-        <button class="loginbtn">Arma-Life Homepage</button>
+    <a href="<?= Config::$forums_url; ?>">
+        <button class="loginbtn"><?= Config::$name ?>Homepage</button>
 
     </a>
 </div>
